93691e0d511782daf83b5d75254c0c107a9014a9
Aki Reijonen <aki.reijonen@gmail.com>
2010-11-06 19:25:40 +0200

    clamp player rotation

--- src/player.cpp
+++ src/player.cpp
@@ -2,11 +2,14 @@
 
 #include "LinearMath/btQuaternion.h"
 
+#include <algorithm>
+
 Player::Player() : Camera(), m_theta(0), m_phi(0) {
 }
 
 void Player::rotate(float x, float y) {
   m_theta -= y/50; m_phi -= x/50;
+  m_theta = std::max(-M_PI_2+1e-5, std::min(M_PI_2-1e-5, (double)m_theta));
   m_center = m_eye + btVector3(0, 1, 0).rotate(btVector3(1, 0, 0), m_theta).rotate(btVector3(0, 0, 1), m_phi);
 
   //m_center = m_center.rotate(btVector3(1, 0, 0), y).rotate(btVector3(0, 0, 1), x);
